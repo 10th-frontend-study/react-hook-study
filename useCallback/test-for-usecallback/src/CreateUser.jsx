df07a4974fcdb83f8fc480d421bfac122eec4a4e
이원주 <turtle4265@naver.com>
2023-08-31 17:15:28 +0900

    feat #7 : [useCallback] md 추가 및 프로젝트 업로드

---
+++ useCallback/test-for-usecallback/src/CreateUser.jsx
@@ -0,0 +1,31 @@
+// import React from "react";
+
+// const CreateUser = React.memo(function CreateUser({
+//   username,
+//   email,
+//   onChange,
+//   onCreate,
+// }) {
+  const CreateUser = ({ username, email, onChange, onCreate }) => {
+  console.log("Createuser rendered...");
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
+  };
+// });
+
+export default CreateUser;
